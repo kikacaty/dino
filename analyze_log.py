104d04475b70be58d7fd9acf2c7a6c745f29f610
Chaowei Xiao <xiaocw@umich.edu>
2021-09-22 23:41:23 -0400

    add imagenet-c

---
+++ analyze_log.py
@@ -0,0 +1,58 @@
+import numpy as np
+import sys, os
+import re
+
+from pdb import set_trace as st
+
+CORRUPTIONS = [
+        'gaussian_noise', 'shot_noise', 'impulse_noise', 'defocus_blur',
+        'glass_blur', 'motion_blur', 'zoom_blur', 'snow', 'frost', 'fog',
+        'brightness', 'contrast', 'elastic_transform', 'pixelate',
+        'jpeg_compression'
+    ]
+
+KNN_PARAMS = ['10','20','100','200']
+
+def compute_mce(corruption_accs):
+    """Compute mCE (mean Corruption Error) normalized by AlexNet performance."""
+    mce = 0.
+    ALEXNET_ERR = [
+        0.886428, 0.894468, 0.922640, 0.819880, 0.826268, 0.785948, 0.798360,
+        0.866816, 0.826572, 0.819324, 0.564592, 0.853204, 0.646056, 0.717840,
+        0.606500
+    ]
+    for i in range(len(CORRUPTIONS)):
+        avg_err = 100 - np.mean(corruption_accs[CORRUPTIONS[i]],axis=1)
+        ce = avg_err / ALEXNET_ERR[i]
+        mce += ce / 15
+    return mce
+
+def analyze_log(log_name):
+    with open(log_name) as f:
+        txt = f.read()
+    m = re.findall(r'.+ Top1: (\d+\.\d+), Top5: (\d+\.\d+)', txt)
+    cnt = 0
+    mCE_list = np.zeros(4)
+    corruption_accs = {}
+    for c in CORRUPTIONS:
+        # print(c)
+        ACC = np.zeros([4,5])
+        for s in range(5):
+            for i in range(4):
+                ACC[i,s] = float(m[cnt][0])
+                cnt += 1
+        corruption_accs[c] = ACC
+        # for k in range(4):
+        #     print(f"{KNN_PARAMS[k]}-NN classifier result: mCE: {mCE[k]}")
+        # mCE_list += mCE
+    
+    mces = compute_mce(corruption_accs)
+
+    for k in range(4):
+            print(f"{KNN_PARAMS[k]}-NN classifier result: total mCE: {mces[k]}")
+
+
+
+if __name__ == '__main__':
+    log_name = sys.argv[1]
+    analyze_log(log_name)
