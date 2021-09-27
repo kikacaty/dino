015a97e631139e92efa9181a855d013c20f23af3
Chaowei Xiao <xiaocw@umich.edu>
2021-09-27 02:41:13 -0400

    update

--- run_eval_lincls.sh
+++ run_eval_lincls.sh
@@ -1,4 +1,5 @@
 CUDA_VISIBLE_DEVICES=3 python -m torch.distributed.launch --nproc_per_node=1 eval_linear.py \
     --data_path /home/scratch.sysarch_nvresearch/chaowei/datasets/ILSVRC2012 \
+    --patch_size 8 \
     --pretrained_weights models/dino_deitsmall8_pretrain_full_checkpoint.pth \
     --lincls_checkpoint models/lincls/dino_deitsmall8_linearweights.pth 
