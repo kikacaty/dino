2e755e52aab542ad53844b773cd5f94aaa551cc0
Chaowei Xiao <xiaocw@umich.edu>
2021-09-27 14:55:46 -0400

    update

--- run_eval_lincls.sh
+++ run_eval_lincls.sh
@@ -12,6 +12,6 @@
 
 CUDA_VISIBLE_DEVICES=3 python -m torch.distributed.launch --nproc_per_node=1 eval_linear.py \
     --data_path /home/scratch.sysarch_nvresearch/chaowei/datasets/ILSVRC2012 \
-    --patch_size 16 --arch vit_base --avgpool_patchtokens True\
+    --patch_size 16 --arch vit_base --avgpool_patchtokens True --n_last_blocks 1\
     --pretrained_weights models/dino_vitbase16_pretrain_full_checkpoint.pth \
     --lincls_checkpoint models/lincls/dino_vitbase16_linearweights.pth | tee vit_b16_lincls.log
